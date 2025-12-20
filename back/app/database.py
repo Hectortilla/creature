021e37318d7531b74cdbdd89bbda2efe9b6faf25
Hector Soria Villalva <hector.soria@clio.com>
2025-12-20 22:26:42 +0100

    Disabling initial database creation: alembic takes care of it

--- back/app/database.py
+++ back/app/database.py
@@ -12,7 +12,8 @@ engine = create_engine(settings.database_url, echo=True)
 
 def create_db_and_tables():
     """Create all database tables."""
-    SQLModel.metadata.create_all(engine)
+    return
+    # SQLModel.metadata.create_all(engine)
 
 
 def get_db_session():
