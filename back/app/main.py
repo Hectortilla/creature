f96f3f83891c29e5551533f824ddc3e3c7fd7f91
Hector Soria Villalva <hector.soria@clio.com>
2026-05-18 21:59:16 +0200

    CORS fix

--- back/app/main.py
+++ back/app/main.py
@@ -42,8 +42,8 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=False,
+    allow_origin_regex=".*",
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
